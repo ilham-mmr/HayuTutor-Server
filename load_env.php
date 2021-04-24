2384bc8797cf72b0c4704b6f047851999a22cd31
Ilham MMR <ilham.mmr@gmail.com>
2021-04-24 12:00:44 +0700

    remove unused lines

--- load_env.php
+++ load_env.php
@@ -6,8 +6,3 @@ use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-echo $_ENV['SERVER_NAME'];
-echo $_ENV['DB_USER'];
-echo $_ENV['DB_PASSWORD'];
-echo $_ENV['DB_NAME'];
