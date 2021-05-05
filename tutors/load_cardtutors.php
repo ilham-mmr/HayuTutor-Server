c60df43c0fdc7a98c60c17c698c9c72086cc2838
Ilham MMR <ilham.mmr@gmail.com>
2021-05-06 06:28:54 +0700

    add load card tutors

--- tutors/load_cardtutors.php
+++ tutors/load_cardtutors.php
@@ -1,7 +1,7 @@
 <?php
 include_once("../dbconnect.php");
 
-$stmt = $conn->prepare("SELECT users.full_name, users.picture, user_tutor.location, sessions.price FROM users INNER JOIN user_tutor ON users.id = user_tutor.user_id INNER JOIN sessions ON user_tutor.tutor_id = sessions.tutor_id LIMIT 0, 3");
+$stmt = $conn->prepare("SELECT users.full_name, users.picture, user_tutor.location, sessions.price, sessions.subject FROM users INNER JOIN user_tutor ON users.id = user_tutor.user_id INNER JOIN sessions ON user_tutor.tutor_id = sessions.tutor_id LIMIT 0, 3");
 $stmt->execute();
 $result = $stmt->get_result();
 
@@ -9,9 +9,11 @@ if ($result->num_rows > 0) {
     $response["tutors"] = array();
     while ($row = $result->fetch_assoc()) {
         $tutorList['full_name'] = $row['full_name'];
-        $tutorList['picture'] = 'https://luxfortis.studio/app/images/profile_pictures/'.$row['picture'];
+        $tutorList['picture'] = 'https://luxfortis.studio/app/images/profile_pictures/' . $row['picture'];
         $tutorList['location'] = $row['location'];
         $tutorList['price'] = $row['price'];
+        $tutorList['subject'] = $row['subject'];
+
 
         array_push($response['tutors'], $tutorList);
     }
