4e013bfd3353126d23563e449b9c025eb5229b61
Ilham MMR <ilham.mmr@gmail.com>
2021-06-07 16:22:58 +0700

    refactor the sql statement

--- tutors/load_cardtutors.php
+++ tutors/load_cardtutors.php
@@ -1,26 +1,32 @@
 <?php
 include_once("../dbconnect.php");
 
-$stmt = $conn->prepare("SELECT users.full_name, users.picture, user_tutor.location, sessions.price, sessions.subject FROM users INNER JOIN user_tutor ON users.id = user_tutor.user_id INNER JOIN sessions ON user_tutor.tutor_id = sessions.tutor_id LIMIT 0, 3");
+$stmt = $conn->prepare("SELECT users.id, users.full_name, users.picture, sessions.price, sessions.subject, sessions.location FROM users INNER JOIN sessions ON users.id = sessions.tutor_id GROUP BY users.id LIMIT 0, 15");
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    $response["tutors"] = array();
-    while ($row = $result->fetch_assoc()) {
-        $tutorList['full_name'] = $row['full_name'];
-        $tutorList['picture'] = 'https://luxfortis.studio/app/images/profile_pictures/' . $row['picture'];
-        $tutorList['location'] = $row['location'];
-        $tutorList['price'] = $row['price'];
-        $tutorList['subject'] = $row['subject'];
+  $response["tutors"] = array();
+  while ($row = $result->fetch_assoc()) {
+    $tutorList['tutorId'] = $row['id'];
+    $tutorList['full_name'] = $row['full_name'];
+    $tutorList['picture'] = 'https://luxfortis.studio/app/images/profile_pictures/' . $row['picture'];
+    $tutorList['location'] = $row['location'];
+    $tutorList['price'] = $row['price'];
+    $tutorList['subject'] = $row['subject'];
 
 
-        array_push($response['tutors'], $tutorList);
-    }
-    sendJsonResponse($response);
+    array_push($response['tutors'], $tutorList);
+  }
+  http_response_code(200);
+  sendJsonResponse($response);
+} else {
+  http_response_code(404);
+  echo 'failed';
+  return;
 }
 
 function sendJsonResponse($sentArray) {
-    header('Content-Type: application/json');
-    echo json_encode($sentArray);
+  header('Content-Type: application/json');
+  echo json_encode($sentArray);
 }
